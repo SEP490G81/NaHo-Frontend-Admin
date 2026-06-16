fa7da6f2b6d594f63083fefb0cba9b95f7156dfd
NhanPhan <ptn987654321@gmail.com>
2026-06-16 22:22:51 +0700

    #1 - feature: add access token constant and problem detail response type

--- types/responses/user.response.ts
+++ types/responses/user.response.ts
@@ -3,13 +3,12 @@ import { Gender, JlptLevel, UserStatus } from "@/types/enums/user.enum";
 export interface UserResponse {
     id: number;
     username: string;
-    roleNames: string[];
-    avatarFileUrl: string | null;
     email: string;
-    firstName: string;
-    lastName: string;
-    gender: Gender;
-    dob: string;
+    roleNames: string[];
+    fullName: string | null;
+    gender: Gender | null;
+    dob: string | null;
+    avatarUrl: string | null;
     jlptLevel: JlptLevel;
     status: UserStatus;
 }
